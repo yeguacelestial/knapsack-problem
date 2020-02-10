c47a146e37c696711c8eaf6a566ea8d4734fb9d3
Yegua Celestial <yeguacelestial@gmail.com>
2020-02-10 19:57:53 +0000

    Created knapsack module

---
+++ knapsack.py
@@ -0,0 +1,26 @@
+# Input -> * Knapsack Weight (ksw)
+#       -> * 'i' element weight (wi)
+#       -> * 'i' element value (vi)
+#       -> * number of items (n)
+#       -> * pelos.dat file
+# Output -> Objective function (objF)
+#        -> Number of items (nItems)
+
+def knapsack(ksw, wi, vi, n):
+    K = [[0 for x in range(ksw+1)] for x in range(n+1)]
+
+    # Build table K[][] in bottom-up manner
+    for i in range(n+1):
+        for w in range(ksw+1):
+
+            # Base case
+            if i==0 or w==0:
+                K[i][w] = 0
+            
+            # Other cases
+            elif wi[i-1] <= w:
+                K[i][w] = max(vi[i-1] + K[i-1][w-wi[i-1]], K[i-1][w])
+            else:
+                K[i][w] = K[i-1][w]
+            
+    return K[n][ksw]
